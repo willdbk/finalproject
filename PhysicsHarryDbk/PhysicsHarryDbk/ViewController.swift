95fe09206d00b8eec9fdbab808400e6d8b500c5c
willdbk <willdbk@gmail.com>
2016-05-09 08:28:39 -0700

    added initial xcode project

---
+++ PhysicsHarryDbk/PhysicsHarryDbk/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PhysicsHarryDbk
+//
+//  Created by Mobile on 4/26/16.
+//  Copyright © 2016 com.dbk.coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let titleLabel = UILabel()
+        titleLabel.text = "Physics App"
+        titleLabel.center = CGPointMake(self.view.size., self.view.y)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
